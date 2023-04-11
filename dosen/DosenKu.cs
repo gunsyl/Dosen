de6a1902803a6859028df19aaa51ef0225951e86
nazladiohevin <nazlahevinbusiness@gmail.com>
2023-04-11 09:41:22 +0700

    rewrite

---
+++ dosen/DosenKu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dosen {
+    public class DosenKu
+    {
+        private string nama;
+        private string nik;
+        private string gender;
+        private string course;
+
+        public DosenKu(string nama, string nik, string gender, string course)
+        {
+            this.nama = nama;
+            this.nik = nik;
+            this.gender = gender;
+            this.course = course;
+        }
+    }
+
+}
--- dosen/Program.cs
+++ dosen/Program.cs
@@ -5,20 +5,15 @@ namespace Dosen
     {
         static void Main(string[] args)
         {
-            Dosen dosen = new Dosen ();
+            DosenKu dosen = new DosenKu("Dosen pengampu", "12345", "Male", "OOP");
 
-            Console.WriteLine("Default Dosen Pengampu: ");
-            dosen.display ();
+            Console.WriteLine("Default Dosen Pengampu: ");       
 
-            Console.WriteLine("\nMasukkan Nama dan NIK Dosen Baru: ");
-            dosen.attDosen(Console.ReadLine(),Convert.ToInt32(Console.ReadLine()));
-            dosen.display ();
+            Console.WriteLine("\nMasukkan Nama dan NIK Dosen Baru: ");   
 
-            Console.WriteLine("\nMasukkan Mata Kuliah Baru: ");
-            dosen.addCourse(Console.ReadLine());
-            dosen.display();
+            Console.WriteLine("\nMasukkan Mata Kuliah Baru: ");            
 
             Console.WriteLine();
         }
-    }
+    } 
 }
