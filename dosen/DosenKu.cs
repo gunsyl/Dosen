53f7f16680a1784adc888e0bbe8c45f556b1e6cb
gunsyl <sylvestergunawan8@gmail.com>
2023-04-11 10:52:12 +0700

    method adding

--- dosen/DosenKu.cs
+++ dosen/DosenKu.cs
@@ -2,18 +2,32 @@
 
 namespace Dosen {
     public class DosenKu
+    
     {
-        private string nama;
-        private string nik;
-        private string gender;
-        private string course;
+        public int id;
+        public string nama;
+        public string nik;
+        public string gender;
+        public string course;
 
-        public DosenKu(string nama, string nik, string gender, string course)
+        
+
+        public DosenKu(int id,string nama, string nik, string gender, string course)
         {
+            this.id = id;
             this.nama = nama;
             this.nik = nik;
             this.gender = gender;
             this.course = course;
+        }
+
+        public void display()
+        {
+            Console.WriteLine($"ID : {this.id}");
+            Console.WriteLine($"Nama : {this.nama}");
+            Console.WriteLine($"NIK : {this.nik}");
+            Console.WriteLine($"Gender : {this.gender}");
+            Console.WriteLine($"Course : {this.course}");
         }
     }
 
--- dosen/Program.cs
+++ dosen/Program.cs
@@ -1,17 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
 namespace Dosen
 {
     class program
     {
         static void Main(string[] args)
         {
-            DosenKu dosen = new DosenKu("Dosen pengampu", "12345", "Male", "OOP");
 
-            Console.WriteLine("Default Dosen Pengampu: ");       
+            DosenKu dosen = new DosenKu(1,"Dosen pengampu", "12345", "Male", "Pemrograman Berorientasi Objek");
+
+            Console.WriteLine("Default Dosen Pengampu: \n");
+
+            dosen.display();
 
             Console.WriteLine("\nMasukkan Nama dan NIK Dosen Baru: ");   
 
-            Console.WriteLine("\nMasukkan Mata Kuliah Baru: ");            
+            //Console.WriteLine("\nMasukkan Mata Kuliah Baru: ");            
 
             Console.WriteLine();
         }
